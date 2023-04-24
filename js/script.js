f163788ca94f001846eca74bb7216139f7046af5
Natan <nfsl@cin.ufpe.br>
2023-04-24 12:30:48 -0300

    feat: sky/floor movement

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const score = document.querySelector('.score')
 const bestScore = document.querySelector('.best__score')
+const sky = document.querySelector('.clouds')
+const sky_1 = document.querySelector('.clouds_1')
+const floor = document.querySelector('.floor' )
+const floor_1 = document.querySelector('.floor_1')
 
 var gameover = false
 
@@ -12,16 +16,30 @@ const jump = () => {
 
         mario.classList.remove('jump');
 
-    }, 1000);
+    }, 750);
 }
 
 const loop = setInterval(() =>{
-
+    const skyPosition = sky.getBoundingClientRect().left;
+    const sky_1Position = sky_1.getBoundingClientRect().left;
     const pipePosition =  pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
     if (pipePosition <= 100 && pipePosition >= 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left= `${pipePosition}px`;
+
+   
+
+        sky.style.transform= `translate(${skyPosition})`;
+        sky.style.animation = 'none';
+       
+
+
+        sky_1.style.transform= `translate(${sky_1Position})`;
+        sky_1.style.animation = 'none';
+        
+
+        console.log(skyPosition,sky_1Position)
         
     }
 
